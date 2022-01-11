f4b8d1e4d867425d10caa460a02f3300b27c2c4b
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-11 09:01:19 +0300

    add

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,15 @@
 function add(param1: number, param2: number): number {
-
+  return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
+  let sum = 0;
+  param1.forEach((v) => (sum += v));
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+  return sum;
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
