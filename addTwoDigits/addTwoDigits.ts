1660158ee50d83411d9630e550cf8abd3e18c2d6
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-12 09:33:33 +0300

    add Two Digits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,6 @@
 function addTwoDigits(n: any): number {
-
+  const nums = n.toString().split('');
+  return nums.reduce((a: string, b: string) => parseInt(a) + parseInt(b));
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
