8e08c4690470f73cad1459d2ff6b4adf81a4d08b
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-17 07:42:50 +0300

    array Change

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 function arrayChange(inputArray: number[]): number {
- 
+  let count = 0;
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      count += inputArray[i - 1] - inputArray[i] + 1;
+      inputArray[i] = inputArray[i - 1] + 1;
+    }
+  }
+  return count;
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 2, 1]));
