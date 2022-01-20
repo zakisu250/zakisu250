6c0aad05aef6a44b50f872bef5d7c956fd58d2af
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-20 09:49:55 +0300

    array Replace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  return inputArray.map((elem) => {
+    if (elem === elemToReplace) {
+      return substitutionElem;
+    }
+    return elem;
+  });
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
