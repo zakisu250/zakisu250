1068ba49b062839c6c77e1452931f32ca73e7d09
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-23 10:16:33 +0300

    Add case-insensitive palindrome check

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,5 +1,8 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
+  const originalLowerCase = inputString.toLowerCase();
+  const reversedWord = originalLowerCase.split('').reverse().join('');
 
+  return originalLowerCase === reversedWord;
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
