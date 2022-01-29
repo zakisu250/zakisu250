3259b7cc8d24453de0da6a0a39e3de924a81006f
zakisudev@gmail.com <zakisu250@gmail.com>
2022-01-29 10:19:34 +0300

    Add logic to determine if two chess board cells have the same color

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,5 +1,15 @@
 function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const board = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const cell1X = board.indexOf(cell1[0]);
+  const cell1Y = parseInt(cell1[1]);
+  const cell2X = board.indexOf(cell2[0]);
+  const cell2Y = parseInt(cell2[1]);
 
+  if (Math.abs(cell1X - cell2X) % 2 === Math.abs(cell1Y - cell2Y) % 2) {
+    return true;
+  }
+
+  return false;
 }
 
 console.log(chessBoardCellColor('A1', 'C3'));
