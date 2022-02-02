99e567bb57ca2cbab8a950af9ea09d9885acd121
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-02 10:22:49 +0300

    Add commonCharacterCount function to count common characters between two strings

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,16 @@
 function commonCharacterCount(s1: string, s2: string): number {
-   
+  let count = 0;
+  const s1Arr = s1.split('');
+  const s2Arr = s2.split('');
+
+  s1Arr.forEach((char) => {
+    if (s2Arr.includes(char)) {
+      count++;
+      s2Arr.splice(s2Arr.indexOf(char), 1);
+    }
+  });
+
+  return count;
 }
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
