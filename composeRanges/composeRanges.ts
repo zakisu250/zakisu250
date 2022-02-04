fd2012b7844a660a9cb1f6447f9e9977931fa81e
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-04 10:24:56 +0300

    Refactor composeRanges function to handle consecutive numbers and add missing newline at end of file

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,6 +1,25 @@
 function composeRanges(nums: number[]): string[] {
+  const ranges = [];
+  let start = 0;
+  let end = 0;
 
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] === 1) {
+      end = i;
+    } else {
+      ranges.push(
+        start === end ? `${nums[start]}` : `${nums[start]}->${nums[end]}`
+      );
+      start = i;
+      end = i;
+    }
+  }
+
+  ranges.push(
+    start === end ? `${nums[start]}` : `${nums[start]}->${nums[end]}`
+  );
+
+  return ranges;
 }
-
 
 console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
