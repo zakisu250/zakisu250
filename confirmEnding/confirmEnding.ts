16bbe9c72bc5a5bf416b0708ab44fd8ed3a091a3
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-05 10:25:37 +0300

    Refactor confirmEnding function to use str.endsWith() method

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,6 @@
 function confirmEnding(str: string, target: string) {
-   
+  return str.endsWith(target);
 }
 
-console.log(confirmEnding("Abstraction", "action"));
-console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'));
+console.log(confirmEnding('Open sesame', 'pen'));
