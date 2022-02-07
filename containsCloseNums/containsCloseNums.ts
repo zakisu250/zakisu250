21816bb2493e07178c5891c3d3c1fb6c4aedf282
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-07 10:27:49 +0300

    Refactor containsCloseNums function to use a Map for efficient lookup

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,16 @@
 function containsCloseNums(nums: number[], k: number): boolean {
+  const numsMap = new Map<number, number>();
 
+  for (let i = 0; i < nums.length; i++) {
+    if (numsMap.has(nums[i])) {
+      if (i - numsMap.get(nums[i])! <= k) {
+        return true;
+      }
+    }
+    numsMap.set(nums[i], i);
+  }
+
+  return false;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
