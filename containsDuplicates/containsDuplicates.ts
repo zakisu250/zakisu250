84026611f3730bb9be2f99fd47cf5b11911b9193
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-10 10:28:25 +0300

    Add duplicate check using Set

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,5 +1,14 @@
 function containsDuplicates(a: number[]): boolean {
+  const numSet = new Set<number>();
 
+  for (let i = 0; i < a.length; i++) {
+    if (numSet.has(a[i])) {
+      return true;
+    }
+    numSet.add(a[i]);
+  }
+
+  return false;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
