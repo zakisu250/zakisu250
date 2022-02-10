2aa48223394cc0214650e6e052dd3634b48c47be
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-10 10:30:30 +0300

    Add string conversion function to check if one string can be formed from another

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,14 @@
 function convertString(s: string, t: string): boolean {
-
+  let tIndex = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t[tIndex]) {
+      tIndex++;
+    }
+    if (tIndex === t.length) {
+      return true;
+    }
+  }
+  return false;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
