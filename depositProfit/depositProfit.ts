36e9b41ec5eb1800e893189b0d58c0f4729c3502
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-13 10:31:53 +0300

    Refactor depositProfit function to calculate the number of years needed to reach the threshold

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,15 @@
-function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+function depositProfit(
+  deposit: number,
+  rate: number,
+  threshold: number
+): number {
+  let years = 0;
+  let balance = deposit;
+  while (balance < threshold) {
+    balance += balance * (rate / 100);
+    years++;
+  }
+  return years;
 }
 
-console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170));
