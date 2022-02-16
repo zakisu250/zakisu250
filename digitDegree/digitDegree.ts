7e7eb4b7f769df5862372d985caabd255f915a40
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-16 10:33:16 +0300

    Refactor digitDegree function to improve readability and maintainability

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,21 @@
 function digitDegree(n: number): number {
-  
+  let count = 0;
+
+  if (n < 10) {
+    return count;
+  }
+
+  while (true) {
+    count++;
+    n = n
+      .toString()
+      .split('')
+      .reduce((a, b) => parseInt(a) + parseInt(b), 0);
+
+    if (n < 10) {
+      return count;
+    }
+  }
 }
 
 console.log(digitDegree(5));
