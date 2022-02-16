9a1677eb829373bf5df1d62592413c498bf11687
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-16 10:36:18 +0300

    Add domainType function to categorize domains

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,29 @@
 function domainType(domains: string[]): string[] {
+  const domainTypes: string[] = [];
 
+  domains.forEach((domain) => {
+    const domainArr = domain.split('.');
+    const domainType = domainArr[domainArr.length - 1];
+
+    switch (domainType) {
+      case 'org':
+        domainTypes.push('organization');
+        break;
+      case 'com':
+        domainTypes.push('commercial');
+        break;
+      case 'net':
+        domainTypes.push('network');
+        break;
+      case 'info':
+        domainTypes.push('information');
+        break;
+    }
+  });
+
+  return domainTypes;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+);
