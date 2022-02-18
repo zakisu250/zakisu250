ec8a0660c143f98de952176b48c1831d2cc04bb4
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-18 10:37:07 +0300

    Add logic to calculate election winners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,12 @@
 function electionsWinners(votes: number[], k: number): number {
+  const max = Math.max(...votes);
+  const count = votes.filter((vote) => vote + k > max).length;
 
+  if (count === 0 && votes.filter((vote) => vote === max).length > 1) {
+    return 0;
+  }
+
+  return count;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
