68ea97a405071053c97b3b5d99ca5c73203ab57a
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-21 10:40:11 +0300

    Add factorializeANumber function with loop implementation

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,11 @@
 function factorializeANumber(num: number): number {
+  let result = 1;
 
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 }
 
 console.log(factorializeANumber(5));
