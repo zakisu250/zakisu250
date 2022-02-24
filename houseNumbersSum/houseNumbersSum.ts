19ec44941077e942ac417bc9ab64b94797976693
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-24 10:49:41 +0300

    Refactor houseNumbersSum function to calculate the sum of numbers before the first occurrence of 0

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,5 @@
 function houseNumbersSum(inputArray: number[]): number {
-
+  return inputArray.slice(0, inputArray.indexOf(0)).reduce((a, b) => a + b, 0);
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
