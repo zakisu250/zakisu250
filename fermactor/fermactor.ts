b727671aac6d77dab801ac6d4427af2138b40901
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-26 10:41:53 +0300

    Refactor fermactor function to improve performance

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,13 @@
 function fermactor(n: number): number[] {
-
+  for (let i = 1; i < n; i++) {
+    for (let j = 1; j < i; j++) {
+      const total = i ** 2 - j ** 2;
+      if (total === n) {
+        return [i, j];
+      }
+    }
+  }
+  return [0, 0];
 }
 
 console.log(fermactor(15));
