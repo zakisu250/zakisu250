d00e4a6be9fbae6106d5f3c9221829eafb2f05e6
zakisudev@gmail.com <zakisu250@gmail.com>
2022-02-28 10:42:25 +0300

    Add logic to find the closest pair of numbers that sum up to a given value

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,5 +1,20 @@
 function findClosestPair(numbers: number[], sum: number): number {
-   
+  let distance = -1;
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const distanceSum = numbers[i] + numbers[j];
+      const absDistance = Math.abs(i - j);
+
+      if (distanceSum === sum) {
+        if (distance === -1 || absDistance < distance) {
+          distance = absDistance;
+        }
+      }
+    }
+  }
+
+  return distance;
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
