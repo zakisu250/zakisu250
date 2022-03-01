64184298602d8865f0dcf0105b366ac44eb6dcbe
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-01 10:43:27 +0300

    Add logic to find the first digit in a string

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,5 +1,10 @@
 function firstDigit(inputString: string): string {
-
+  for (let i = 0; i < inputString.length; i++) {
+    if (!isNaN(parseInt(inputString[i]))) {
+      return inputString[i];
+    }
+  }
+  return '';
 }
 
 console.log(firstDigit('var_1__Int'));
