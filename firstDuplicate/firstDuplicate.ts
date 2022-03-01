2d32755159a656ace5e0f8f6995ada2dc1f6cbe8
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-01 10:44:29 +0300

    Add firstDuplicate function to find the first duplicate number in an array

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,5 +1,12 @@
 function firstDuplicate(a: number[]): number {
-    
+  const numSet = new Set();
+  for (let i = 0; i < a.length; i++) {
+    if (numSet.has(a[i])) {
+      return a[i];
+    }
+    numSet.add(a[i]);
+  }
+  return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
