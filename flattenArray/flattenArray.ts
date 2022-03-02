8b8486b6426020f29ba6a7d61271e4544db3d570
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-02 10:48:24 +0300

    Refactor flattenArray function to handle nested arrays

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,14 @@
 function flattenArray(arr: any[]): any[] {
-
+  let result: any[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flattenArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
-console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([[['a']], [['b']]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
