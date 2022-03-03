000130c6bb62bd6f0f05bb881a9bedd68bedee2c
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-03 10:48:53 +0300

    Refactor growingPlant function to improve readability and add missing newline at end of file

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,19 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+function growingPlant(
+  upSpeed: number,
+  downSpeed: number,
+  desiredHeight: number
+): number {
+  let days = 0;
+  let height = 0;
+  while (height < desiredHeight) {
+    height += upSpeed;
+    days++;
+    if (height >= desiredHeight) {
+      return days;
+    }
+    height -= downSpeed;
+  }
+  return days;
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
