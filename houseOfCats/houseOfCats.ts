e4b2156b47c3b3c8173e2206b04b85ddbf221383
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-04 10:51:17 +0300

    Add logic to calculate the number of people in the house based on the number of legs

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,5 +1,13 @@
 function houseOfCats(legs: number): number[] {
+  const people = [];
 
+  for (let i = 0; i <= legs / 2; i++) {
+    if ((legs - 2 * i) % 4 === 0) {
+      people.push((legs - 2 * i) / 4);
+    }
+  }
+
+  return people;
 }
 
 console.log(houseOfCats(6));
