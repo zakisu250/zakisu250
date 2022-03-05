217a4949c37069cf8de3001f2ff5bdd7b5d34a29
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-05 10:52:04 +0300

    Add logic to generate HTML end tag from start tag

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,12 @@
 function htmlEndTagByStartTag(startTag: string): string {
-
+  const startTagArr = startTag.split(' ');
+  const startTagStr = startTagArr[0].split('');
+  let endTag = '</';
+  for (let i = 1; i < startTagStr.length; i++) {
+    endTag += startTagStr[i];
+  }
+  return endTag[endTag.length - 1] === '>' ? endTag : endTag + '>';
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag('<i>'));
