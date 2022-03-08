1925cc86d500047d0c8173607b352d51ab75a419
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-08 10:56:53 +0300

    Refactor isLucky function to calculate the sum of first and second halves of a number

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,16 @@
 function isLucky(n: number): boolean {
-    
+  const numString = n.toString();
+  const firstHalf = numString.slice(0, numString.length / 2);
+  const secondHalf = numString.slice(numString.length / 2);
+
+  const firstHalfSum = firstHalf
+    .split('')
+    .reduce((a, b) => parseInt(a.toString()) + parseInt(b), 0);
+  const secondHalfSum = secondHalf
+    .split('')
+    .reduce((a, b) => parseInt(a.toString()) + parseInt(b), 0);
+
+  return firstHalfSum === secondHalfSum;
 }
 
 console.log(isLucky(1230));
