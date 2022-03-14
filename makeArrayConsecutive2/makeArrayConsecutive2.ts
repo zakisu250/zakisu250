c8f44d86942d2f811448999476ce4032880640aa
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-14 11:02:31 +0300

    Refactor makeArrayConsecutive2 function

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,5 @@
 function makeArrayConsecutive2(statues: number[]): number {
-
+  return Math.max(...statues) - Math.min(...statues) - statues.length + 1;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
