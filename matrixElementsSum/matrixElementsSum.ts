3bfa5d052a4ba5100367b376e4df8ca426d52cba
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-14 11:03:17 +0300

    Add matrixElementsSum function to calculate the sum of matrix elements

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,9 +1,20 @@
 function matrixElementsSum(matrix: any[][]): number {
-
+  let total = 0;
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === 0) {
+        break;
+      }
+      total += matrix[j][i];
+    }
+  }
+  return total;
 }
 
-console.log(matrixElementsSum(
-    [[0, 1, 1, 2],
+console.log(
+  matrixElementsSum([
+    [0, 1, 1, 2],
     [0, 5, 0, 0],
-    [2, 0, 3, 3]]
-));
+    [2, 0, 3, 3],
+  ])
+);
