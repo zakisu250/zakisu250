d1386a429d63fb2023a9035178c6f851ab065c33
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-18 11:05:37 +0300

    Add minimalNumberOfCoins function to calculate the minimal number of coins needed

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,10 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
-
+  let count = 0;
+  for (let i = coins.length - 1; i >= 0; i--) {
+    count += Math.floor(price / coins[i]);
+    price %= coins[i];
+  }
+  return count;
 }
 
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
