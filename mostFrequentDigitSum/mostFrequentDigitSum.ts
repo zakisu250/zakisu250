641238f21f5e86f41d04d5a5473c7ca22e2186fd
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-20 11:07:28 +0300

    Add mostFrequentDigitSum function to calculate the most frequent digit sum

--- mostFrequentDigitSum/mostFrequentDigitSum.ts
+++ mostFrequentDigitSum/mostFrequentDigitSum.ts
@@ -1,5 +1,30 @@
 function mostFrequentDigitSum(n: number): number {
+  const sum = (n: number) => {
+    let sum = 0;
+    while (n) {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    }
+    return sum;
+  };
 
+  const sums: { [key: number]: number } = {};
+  while (n) {
+    const s = sum(n);
+    sums[s] = sums[s] ? sums[s] + 1 : 1;
+    n -= s;
+  }
+
+  let max = 0;
+  let maxKey = 0;
+  for (const key in sums) {
+    if (sums[key] > max) {
+      max = sums[key];
+      maxKey = parseInt(key);
+    }
+  }
+
+  return maxKey;
 }
 
 console.log(mostFrequentDigitSum(88));
