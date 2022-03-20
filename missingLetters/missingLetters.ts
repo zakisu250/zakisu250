791131dc9b6286eeec5844fc5d1498387cb83d0b
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-20 11:06:21 +0300

    Refactor missingLetters function to return undefined when no missing letters found

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,7 +1,14 @@
-function missingLetters(str: string): string {
-   
+function missingLetters(str: string): string | undefined {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const start = alphabet.indexOf(str[0]);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== alphabet[start + i]) {
+      return alphabet[start + i];
+    }
+  }
+  return undefined;
 }
-console.log(missingLetters("bce"));
-console.log(missingLetters("abce"));
-console.log(missingLetters("abcdefghjklmno"));
-console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters('bce'));
+console.log(missingLetters('abce'));
+console.log(missingLetters('abcdefghjklmno'));
+console.log(missingLetters('abcdefghijklmnopqrstuvwxyz'));
