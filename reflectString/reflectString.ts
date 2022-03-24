58c2a571d83847350f21da8705d2449e304487ac
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-24 11:12:51 +0300

    Add reflectString function to reflect a string

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,10 @@
 function reflectString(inputString: string): string {
-  
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let reflected = '';
+  for (let i = 0; i < inputString.length; i++) {
+    reflected += alphabet[25 - alphabet.indexOf(inputString[i])];
+  }
+  return reflected;
 }
 
-console.log(reflectString("name"));
+console.log(reflectString('name'));
