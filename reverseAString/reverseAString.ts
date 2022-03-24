76a81fa5de45e7700cc0918c3bb10f673c9560ab
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-24 11:13:23 +0300

    Add reverseAString function implementation

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,5 +1,9 @@
 function reverseAString(str: string): string {
-
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
 }
 
 console.log(reverseAString('hello'));
