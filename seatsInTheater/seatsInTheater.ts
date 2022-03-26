12ae73e3518b63a5d08b34cdf3d052a596d1275f
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-26 11:14:47 +0300

    Refactor seatsInTheater function signature

--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,10 @@
-function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
-
+function seatsInTheater(
+  nCols: number,
+  nRows: number,
+  col: number,
+  row: number
+): number {
+  return (nCols - col + 1) * (nRows - row);
 }
 
 console.log(seatsInTheater(16, 11, 5, 3));
