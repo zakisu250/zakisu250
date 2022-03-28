3e78301113e9616fec2b9c14c744b83ad71c0bf8
zakisudev@gmail.com <zakisu250@gmail.com>
2022-03-28 11:16:33 +0300

    Refactor sortByLength function to sort inputArray by length

--- sortByLength/sortByLength.ts
+++ sortByLength/sortByLength.ts
@@ -1,9 +1,5 @@
 function sortByLength(inputArray: string[]): string[] {
-
+  return inputArray.sort((a, b) => a.length - b.length);
 }
 
-console.log(sortByLength(["abc",
-"",
-"aaa",
-"a",
-"zz"]));
+console.log(sortByLength(['abc', '', 'aaa', 'a', 'zz']));
