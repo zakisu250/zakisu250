d0338a25fcc240206932830d95f8ac94b00b636a
zakisudev@gmail.com <zakisu250@gmail.com>
2022-04-02 11:22:23 +0300

    Add sumOddFibonacciNums function to calculate the sum of odd Fibonacci numbers

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,5 +1,15 @@
 function sumOddFibonacciNums(num: number): number {
-
+  let sum = 1;
+  let current = 1;
+  let previous = 0;
+  while (current <= num) {
+    if (current % 2 !== 0) {
+      sum += current;
+    }
+    current += previous;
+    previous = current - previous;
+  }
+  return sum;
 }
 
 console.log(sumOddFibonacciNums(1000));
