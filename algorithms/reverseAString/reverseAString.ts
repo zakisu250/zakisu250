25452689472462d8e2a045d70b6533b4a6801989
zakisudev@gmail.com <zakisu250@gmail.com>
2022-04-30 11:46:13 +0300

    Add reverseAString, seatsInTheater, seekAndDestroy, and shapeArea algorithms

---
+++ algorithms/reverseAString/reverseAString.ts
@@ -0,0 +1,10 @@
+function reverseAString(str: string): string {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseAString('hello'));
+console.log(reverseAString('Howdy'));
