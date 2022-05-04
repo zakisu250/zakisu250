224bde6e1b99e8e0a0d7afc4d5e8ca4bc8826821
zakisudev@gmail.com <zakisu250@gmail.com>
2022-05-04 11:47:50 +0300

    Add new algorithms: sumOfTwo, switchLights, tasksType, uniqueDigitProducts

---
+++ algorithms/tasksType/tasksType.ts
@@ -0,0 +1,16 @@
+function tasksTypes(deadlines: number[], day: number): number[] {
+  let [today, upcoming, later] = [0, 0, 0];
+  deadlines.forEach((deadline) => {
+    if (deadline <= day) {
+      today++;
+    } else if (deadline <= day + 7) {
+      upcoming++;
+    } else {
+      later++;
+    }
+  });
+  return [today, upcoming, later];
+}
+
+console.log(tasksTypes([1, 2, 3, 4, 5], 2));
+console.log(tasksTypes([1, 2, 4, 2, 10, 3, 1, 4, 5, 4, 9, 8], 1));
