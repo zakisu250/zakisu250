73526f26a5f9e4370d9156481c2f01fadfec62ce
zakisudev@gmail.com <zakisu250@gmail.com>
2022-05-11 11:51:32 +0300

    Delete unnecessary code files

--- centuryFromYear/centuryFromYear.ts
+++
@@ -1,6 +0,0 @@
-function centuryFromYear(year: number): number {
-  return Math.ceil(year / 100);
-}
-
-console.log(centuryFromYear(1905));
-console.log(centuryFromYear(1700));
--- checkPalindrome/checkPalindrome.ts
+++
@@ -1,9 +0,0 @@
-function checkPalindrome(inputString: string): boolean {
-  const reversedWord = inputString.split('').reverse().join('');
-
-  return inputString === reversedWord;
-}
-
-console.log(checkPalindrome('aabaa'));
-console.log(checkPalindrome('abac'));
-console.log(checkPalindrome('a'));
