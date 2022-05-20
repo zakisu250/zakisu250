e160adee50a51058efd9180e6ccf95df5ffa90b4
zakisudev@gmail.com <zakisu250@gmail.com>
2022-05-20 11:54:18 +0300

    Remove unused code and files

--- findEmailDomain/findEmailDomain.ts
+++
@@ -1,7 +0,0 @@
-function findEmailDomain(address: string): string {
-  const addressParts = address.split('@');
-  return addressParts[addressParts.length - 1];
-}
-
-console.log(findEmailDomain('prettyandsimple@example.com'));
-console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
