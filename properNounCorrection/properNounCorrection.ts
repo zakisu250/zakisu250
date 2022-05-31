417b8aba5255989c82d3b321a0e7b1f61638d155
zakisudev@gmail.com <zakisu250@gmail.com>
2022-05-31 11:57:17 +0300

    move to algorithm folder

--- properNounCorrection/properNounCorrection.ts
+++
@@ -1,6 +0,0 @@
-function properNounCorrection(noun: string): string {
-  return noun[0].toUpperCase() + noun.slice(1).toLowerCase();
-}
-
-console.log(properNounCorrection('pARiS'));
-console.log(properNounCorrection('John'));
