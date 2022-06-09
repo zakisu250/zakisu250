5cde82ca2ac9da9612d2e757afc316bdfee2b3ea
zakisudev@gmail.com <zakisu250@gmail.com>
2022-06-09 12:00:00 +0300

    move to algorithm folder

--- validTime/validTime.ts
+++
@@ -1,8 +0,0 @@
-function validTime(time: string): boolean {
-  const [hours, minutes] = time.split(':').map(Number);
-  return hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60;
-}
-
-console.log(validTime('13:58'));
-console.log(validTime('25:51'));
-console.log(validTime('02:76'));
