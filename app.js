883645b494dd9c358b34f982f0a733503439c2a2
zakisudev@gmail.com <zakisu250@gmail.com>
2023-01-01 10:10:32 +0300

    arrow function

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ export function subtract(a, b) {
   return a - b;
 }
 
+// arrow function
+const multiply = (a, b) => a * b;
+
 console.log(sum(1, 2));
 console.log(subtract(1, 2));
+console.log(multiply(1, 2));
