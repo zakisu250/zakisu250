b6363d1496b86dea4d46696a7327de33831fb588
zakisudev@gmail.com <zakisu250@gmail.com>
2023-01-02 10:20:22 +0300

    unordered list and list items

--- index.jsx
+++ index.jsx
@@ -6,6 +6,13 @@ class MyComponent extends React.Component {
       <div>
         <h1>Hello, World!</h1>
         <p>My name is Zaki.</p>
+
+        <p>My favorite foods are:</p>
+        <ul>
+          <li>Pizza</li>
+          <li>Ice Cream</li>
+          <li>Donuts</li>
+        </ul>
       </div>
     );
   }
