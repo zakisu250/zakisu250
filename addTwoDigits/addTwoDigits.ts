6cd8cab0d1f174294be41ab4494626b03c445148
zakisudev@gmail.com <zakisu250@gmail.com>
2024-02-28 11:31:17 +0300

    Delete unnecessary code files

--- addTwoDigits/addTwoDigits.ts
+++
@@ -1,6 +0,0 @@
-function addTwoDigits(n: any): number {
-  const nums = n.toString().split('');
-  return nums.reduce((a: string, b: string) => parseInt(a) + parseInt(b));
-}
-
-console.log(addTwoDigits(29));
